37afe0fa4e418279d3623968c03071cc35c1f891
Ralph <ralphfield@ostmw0181222.bskyb.com>
2018-05-14 10:49:55 +0100

    First commit
    Working but no ability to export identicon

---
+++ src/components/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+      <h3>My Identicon Generator</h3>
+      <div>
+          Input:
+          <input v-on:input="onInput" />
+      </div>
+      <div>
+          Output: {{ reversal }} <!-- REVERSAL ONLY HERE TO SHOW WHAT CAN BE PUT IN CURLY BRACES -->
+          <div v-html="identicon"></div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            textInput: ''
+        }
+    },
+    computed: {
+        identicon: function() {
+            return jdenticon.toSvg(this.textInput, 300);
+        },
+        // random computed function to show what can be inserted into HTML using the double curly brackets
+        reversal: function() {
+            return this.textInput.split('').reverse().join('');
+        }
+    },
+    methods: {
+        onInput: function(event) {
+            this.textInput = event.target.value;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        font-size: 1em;
+        font-family: 'Gill Sans';
+    }
+
+    h3 {
+        font-size: 2em;
+    }
+</style>
